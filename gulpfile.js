436982bc64aa691f24f9e151f1f3646c40e1321a
ahroidlife <ahroidlife@gmail.com>
2018-09-11 15:41:05 +0700

    front-end transiago

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    autoprefixer = require("gulp-autoprefixer"),
+    browserSync  = require("browser-sync"),
+    webpack      = require("webpack-stream");
+
+gulp.task("sass", function(){
+    return gulp.src("./resource/assets/sass/**/*.scss")
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest("./public/assets/css"))
+})
+
+gulp.task("serve", function(){
+    browserSync.init({
+        server:{
+            baseDir: "./public/"
+        }
+    })
+
+
+    gulp.watch("./resource/assets/sass/**/*.scss", ['sass'])
+    gulp.watch("./public/assets/css/**/*.css").on("change", browserSync.reload)
+    gulp.watch("./public/*.html").on("change", browserSync.reload)
+})
